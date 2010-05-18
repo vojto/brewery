c6b0c953838692234dda19a706ea6a6eaea2e270
Stefan Urbanek <stefan@Stefan-Urbaneks-MacBook.local>
2010-05-19 01:54:15 +0200

    changed brewery project structure

--- lib/brewery.rb
+++ lib/brewery.rb
@@ -1,22 +1,26 @@
-require 'node'
-require 'datastore'
-require 'stream'
+# ETL - main ETL header
+#
+# Copyright:: (C) 2010 Knowerce, s.r.o.
+# 
+# Author:: Stefan Urbanek
+# Date:: May 2010
+#
 
-# Nodes
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-# Input Nodes
-require 'file_source_node'
+require 'rubygems'
 
-# Record Processing Nodes
-require 'merge_node'
-require 'aggregate_node'
-require 'select_node'
-
-# Field Nodes
-require 'derive_node'
-require 'field_map_node'
-# require 'type_node'
-
-# Output Nodes
-require 'table_output_node'
-require 'file_output_node'
+require 'brewery-core'
+require 'brewery-etl'
+# require 'brewery-streams'
